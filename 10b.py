8878b40d0be2c76ab32a3492d6d788f4908d36fb
trina <31892809+kateshostak@users.noreply.github.com>
2020-12-15 04:19:40 +0300

    10.2

--- 10b.py
+++ 10b.py
@@ -1,33 +1,23 @@
+from collections import defaultdict
+
+
 def parse(f):
     inpt = [int(elem) for elem in f.strip().split('\n')]
     inpt.append(0)
     inpt.append(max(inpt) + 3)
-    inpt.sort()
-    w = {}
-    for i, elem in enumerate(inpt):
-        w[elem] = []
-        j = i + 1
-        while j < len(inpt) and inpt[j] - elem <= 3:
-            w[elem].append(inpt[j])
-            j += 1
-    return w
+    return inpt
 
 
 def solution(inpt):
-    def traverse(elem):
-        if elem == 49:
-            return 1
-
-        res = 0
-        for el in inpt[elem]:
-            res += traverse(el)
-        return res
-    print(traverse(0))
-
+    d = defaultdict(int)
+    d[max(inpt)] = 1
+    for elem in sorted(inpt[:-1], reverse=True):
+        d[elem] = d[elem + 1] + d[elem + 2] + d[elem + 3]
+    return d[0]
 
 
 def main():
-    with open('10test.txt') as f:
+    with open('10.txt') as f:
         print(solution(parse(f.read())))
 
 
